fe5e18715f8a615c5930d68e4b036bab754846f8
Iva Kop <iva@penguin.digital>
2018-09-20 23:56:26 +0300

    Implement authentication

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import Login from './src/screens/Login'
 import Secure from './src/screens/Secure'
+import { authEndpoint } from './config.json'
 
 type Props = {}
 
 export default class App extends Component<Props> {
   state = {
     isLoggedIn: false,
-    env: 'production'
+    token: '',
+    env: 'development'
+  }
+
+  login = (username, password) => {
+    fetch(
+      `${
+        authEndpoint[this.state.env]
+      }?username=${username}&password=${password}&grant_type=password`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'Basic YmFyQ2xpZW50SWRQYXNzd29yZDpzZWNyZXQ='
+        }
+      }
+    )
+      .then(data =>
+        this.setState({ token: JSON.parse(data._bodyInit).access_token })
+      )
+      .catch(err => err)
   }
 
   render() {
+    console.log(this.state.token)
     if (this.state.isLoggedIn)
       return (
         <Secure onLogoutPress={() => this.setState({ isLoggedIn: false })} />
@@ -20,7 +41,7 @@ export default class App extends Component<Props> {
         <Login
           env={this.state.env}
           onEnvChange={env => this.setState({ env })}
-          onLoginPress={() => this.setState({ isLoggedIn: true })}
+          onLoginPress={this.login}
         />
       )
   }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -10,20 +10,43 @@ import {
 } from 'react-native'
 
 export default class Login extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
   render() {
     return (
       <ScrollView style={styles.container}>
         <Text style={styles.text}>Login</Text>
-        <TextInput placeholder="Username" style={styles.input} />
-        <TextInput placeholder="Password" style={styles.input} />
-        <Button onPress={this.props.onLoginPress} title="Submit" />
+        <TextInput
+          placeholder="username"
+          autoCapitalize="none"
+          style={styles.input}
+          onChangeText={username =>
+            this.setState({ username: username.toLowerCase() })
+          }
+        />
+        <TextInput
+          secureTextEntry
+          autoCapitalize="none"
+          placeholder="password"
+          style={styles.input}
+          onChangeText={password => this.setState({ password })}
+        />
+        <Button
+          onPress={() =>
+            this.props.onLoginPress(this.state.username, this.state.password)
+          }
+          title="Submit"
+        />
         <Picker
           selectedValue={this.props.env}
           onValueChange={itemValue => this.props.onEnvChange(itemValue)}
         >
           <Picker.Item label="Production" value="production" />
           <Picker.Item label="Demo" value="demo" />
-          <Picker.Item label="Testing" value="js" />
+          <Picker.Item label="Testing" value="testing" />
+          <Picker.Item label="Development" value="development" selectedValue />
         </Picker>
       </ScrollView>
     )
