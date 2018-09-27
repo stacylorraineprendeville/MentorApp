66b85761de046349e5818bfdcdabb5d3478858dc
Iva Kop <iva@penguin.digital>
2018-09-27 19:21:18 +0300

    Add missing env function

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,6 +2,15 @@ import { combineReducers } from 'redux'
 
 export const SET_ENV = 'SET_ENV'
 
+export const env = (state = 'development', action) => {
+  switch (action.type) {
+    case SET_ENV:
+      return action.env
+    default:
+      return state
+  }
+}
+
 export const setEnv = env => ({
   type: SET_ENV,
   env
