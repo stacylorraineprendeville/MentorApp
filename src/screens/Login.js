4dea063516627a0c429c04ec8fafb478b4cd4039
Dan Atanasov <danmanm@gmail.com>
2018-09-27 10:23:16 +0300

    Prepare offline image caching

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
   ScrollView,
   Text,
@@ -6,26 +6,25 @@ import {
   Button,
   StyleSheet,
   Picker
-} from "react-native";
-
-import { connect } from "react-redux";
-import { getSession, setEnv } from "../redux/reducer";
+} from 'react-native'
+import { connect } from 'react-redux'
+import { getSession, setEnv } from '../redux/reducer'
 
 class Login extends Component {
   state = {
-    username: "",
-    password: ""
-  };
+    username: '',
+    password: ''
+  }
 
   onEnvChange = env => {
-    this.props.setEnv(env);
-  };
+    this.props.setEnv(env)
+  }
 
   render() {
-    console.log(this.props);
     return (
       <ScrollView style={styles.container}>
         <Text style={styles.text}>Login</Text>
+
         <TextInput
           placeholder="username"
           autoCapitalize="none"
@@ -49,17 +48,14 @@ class Login extends Component {
           }
           title="Submit"
         />
-        <Picker
-          selectedValue={this.props.env}
-          onValueChange={itemValue => this.onEnvChange(itemValue)}
-        >
+        <Picker selectedValue={this.props.env} onValueChange={this.onEnvChange}>
           <Picker.Item label="Production" value="production" />
           <Picker.Item label="Demo" value="demo" />
           <Picker.Item label="Testing" value="testing" />
           <Picker.Item label="Development" value="development" selectedValue />
         </Picker>
       </ScrollView>
-    );
+    )
   }
 }
 
@@ -70,23 +66,23 @@ const styles = StyleSheet.create({
   text: {
     margin: 30,
     fontSize: 30,
-    textAlign: "center",
+    textAlign: 'center',
     padding: 5
   },
-  input: { fontSize: 20, textAlign: "center" }
-});
+  input: { fontSize: 20, textAlign: 'center' }
+})
 
 const mapStateToProps = ({ env, login }) => ({
   env,
   login
-});
+})
 
 const mapDispatchToProps = {
   getSession,
   setEnv
-};
+}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Login);
+)(Login)
