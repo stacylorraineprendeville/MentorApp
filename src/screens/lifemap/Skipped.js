f507fa5cded4efbd0d2e62bb00d1ffd9c11108ca
Iva Kop <iva@penguin.digital>
2018-11-01 18:57:17 +0200

    Add tip component

--- src/screens/lifemap/Skipped.js
+++ src/screens/lifemap/Skipped.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
-import { StyleSheet, ScrollView, Image } from 'react-native'
+import { StyleSheet, ScrollView, Image, View } from 'react-native'
+import { Divider } from 'react-native-elements'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import Checkbox from '../../components/Checkbox'
+import Tip from '../../components/Tip'
 
 import globalStyles from '../../globalStyles'
 
-import Button from '../../components/Button'
 import colors from '../../theme.json'
 
 export class Skipped extends Component {
   render() {
     return (
-      <ScrollView style={globalStyles.container}>
-        <Image source={require('../../../assets/images/skipped.png')} />
+      <ScrollView style={globalStyles.background}>
+        <View style={globalStyles.container}>
+          <Image
+            style={styles.image}
+            source={require('../../../assets/images/skipped.png')}
+          />
+        </View>
+        <Divider style={{ backgroundColor: colors.lightgrey }} />
+        <Tip
+          title={'You skipped the following questions'}
+          description={'Click on the question to answer it now!'}
+        />
       </ScrollView>
     )
   }
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: { alignSelf: 'center' }
+})
 
 Skipped.propTypes = {
   drafts: PropTypes.array,
