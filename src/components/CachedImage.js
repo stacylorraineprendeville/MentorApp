084b1449d5961310658ae063c44a448584df9396
Dan Atanasov <danmanm@gmail.com>
2018-11-06 17:45:05 +0200

    Use cachedImage component in the slider

--- src/components/CachedImage.js
+++ src/components/CachedImage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Image, StyleSheet, Platform } from 'react-native'
+import { Image, Platform } from 'react-native'
 import PropTypes from 'prop-types'
 import RNFetchBlob from 'rn-fetch-blob'
 
@@ -35,26 +35,15 @@ export class CachedImage extends Component {
   }
   render() {
     const { cachedSource } = this.state
-    const { source } = this.props
+    const { source, style } = this.props
 
-    return (
-      <Image
-        style={styles.container}
-        source={{ uri: cachedSource || source }}
-      />
-    )
+    return <Image style={style} source={{ uri: cachedSource || source }} />
   }
 }
 
 CachedImage.propTypes = {
-  source: PropTypes.string
+  source: PropTypes.string,
+  style: PropTypes.object
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: 100,
-    height: 100
-  }
-})
 
 export default CachedImage
