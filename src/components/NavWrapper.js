e8b856a58627f9cba8eb7aa3ab39bcb93b83531e
Dan Atanasov <danmanm@gmail.com>
2018-12-07 11:23:47 +0200

    Fix #76 - force dark theme on the status bar

--- src/components/NavWrapper.js
+++ src/components/NavWrapper.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getHydrationState } from '../redux/store'
 import { LoginStack, AppStack } from './navigation'
 import Loading from './Loading'
+import colors from '../theme.json'
 
 export class NavWrapper extends Component {
   state = {
@@ -42,12 +43,17 @@ export class NavWrapper extends Component {
     this.clearTimers()
   }
   render() {
-    return this.state.rehydrated ? (
+    return (
       <View style={styles.container}>
-        {this.props.user.token ? <AppStack /> : <LoginStack />}
+        <StatusBar backgroundColor={colors.palebeige} barStyle="dark-content" />
+        {this.state.rehydrated ? (
+          <View style={styles.container}>
+            {this.props.user.token ? <AppStack /> : <LoginStack />}
+          </View>
+        ) : (
+          <Loading time={this.state.loadingTime} />
+        )}
       </View>
-    ) : (
-      <Loading time={this.state.loadingTime} />
     )
   }
 }
