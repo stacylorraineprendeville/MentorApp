30c5f0080da67a23cbda30ef6d5fb42d53a434b6
Dan Atanasov <danmanm@gmail.com>
2018-12-07 10:30:30 +0200

    Remove/comment out unused dependencies and styles

--- src/components/navigation/DrawerNavigator.js
+++ src/components/navigation/DrawerNavigator.js
@@ -3,15 +3,15 @@ import { createDrawerNavigator } from 'react-navigation'
 import { Image, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
 import DrawerContentComponent from './DrawerContent'
-import SyncStack from './SyncStack'
-import FamiliesStack from './FamiliesStack'
+// import SyncStack from './SyncStack'
+// import FamiliesStack from './FamiliesStack'
 import LifemapStack from './LifemapStack'
 import DashboardStack from './DashboardStack'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import i18n from '../../i18n'
 import colors from '../../theme.json'
 import dashboardIcon from '../../../assets/images/icon_dashboard.png'
-import familyNavIcon from '../../../assets/images/icon_family_nav.png'
+// import familyNavIcon from '../../../assets/images/icon_family_nav.png'
 
 // Separate component for the icon next to each nav link as color and size are
 // the same for each.
